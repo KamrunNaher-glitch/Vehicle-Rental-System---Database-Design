313f4a99ecc57fb94e5d698e82ca8c6d0c6c19b1
KamrunNaher-glitch <keyachandani7@gmail.com>
2025-12-23 18:09:59 +0600

    all queries done

---
+++ queries.sql
@@ -0,0 +1,32 @@
+--query1
+SELECT
+  b.booking_id,
+  u.name AS customer_name,
+  v.vehicle_name
+FROM bookings b
+inner join users u ON b.user_id = u.user_id
+inner join vehicles v ON b.vehicle_id = v.vehicle_id;
+
+--query2
+SELECT *
+FROM vehicles v
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM bookings b
+  WHERE b.vehicle_id = v.vehicle_id
+);
+
+--query 3
+SELECT *
+FROM vehicles
+WHERE availability_status = 'available'
+AND vehicle_type = 'car';
+
+--query4
+SELECT 
+  v.vehicle_name,
+  COUNT(b.booking_id) AS total_bookings
+FROM vehicles v
+JOIN bookings b ON v.vehicle_id = b.vehicle_id
+GROUP BY v.vehicle_name
+HAVING COUNT(b.booking_id) > 2;
